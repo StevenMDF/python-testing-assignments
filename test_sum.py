352546c252484ec9818c881b897ca9cbf12951c8
Steven <stevenm@whitecliffe.ac.nz>
2021-08-09 13:46:04 +1200

    Update test_sum.py

--- test_sum.py
+++ test_sum.py
@@ -2,8 +2,8 @@ import unittest
 from task1 import sum
 
 class TESTSUM(unittest.TestCase):
-  def test_sum(self):
-    self.assertEqual(sum(2,3),5, "2 plus 3 should be 5")
+    def test_sum(self):
+        self.assertEqual(sum(2,3),5, "2 plus 3 should be 5")
 
- if __name__=='__main__':
-  unittest.main()
+if __name__=='__main__':
+    unittest.main()
